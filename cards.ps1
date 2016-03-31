6efe17fae8e2edeba25b146b2acec28c13896f2c
JB Lewis <jb.lewis@hennepin.us>
2016-03-31 15:19:10 -0500

    Adding Cards.ps1 for the Playing Cards item

---
+++ cards.ps1
@@ -0,0 +1,30 @@
+﻿$Deck = ("AS","2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS",
+         "AC","2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC",
+         "AH","2H","3H","4H","5H","6H","7H","8H","9H","10H","JH","QH","KH",
+         "AD","2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD")
+
+$count = 7
+
+$hand1 = @()
+$hand2 = @()
+
+$Shuffle = Get-Random -InputObject $Deck -Count 52
+
+$Shuffle -join(",")
+
+$Shuffle1 = $Shuffle
+
+for ($i=1; $i -le $count; $i++)
+{
+    $Deal,$Shuffle1 = $Shuffle1
+    $hand1 = $hand1 + $Deal
+
+    $Deal,$Shuffle1 = $Shuffle1
+    $hand2 = $hand2 + $Deal
+}
+
+$Shuffle1 -join(",")
+
+$hand1 -join(",")
+
+$hand2 -join(",")
